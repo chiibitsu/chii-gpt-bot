e129e67c9e2e9c0c0e2750cb7dc713b255bd9a8f
chiibitsu <chii.crypto.queen@gmail.com>
2023-04-04 17:28:21 +0800

    fixed consistency of code to remove errors

--- keep_alive.js
+++ keep_alive.js
@@ -1,12 +1,14 @@
-const express = require("express");
+import express from 'express';
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("ChiiGPT is awake!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+function keepAlive() {
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+}
 
 export default keepAlive;
