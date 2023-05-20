2804c91334e2c157851e733b639dbece4bd82602
chiibitsu <chii.crypto.queen@gmail.com>
2023-05-20 16:18:15 +0800

    Added conversation history for context to discord users' messages

--- index.js
+++ index.js
@@ -20,19 +20,35 @@ const openai = new OpenAIApi(new Configuration({
 
 console.log("ChiiGPT is connected and ready!"); 
 
+const userConversations = {};
+
 client.on("messageCreate", async function (message) {
   if (message.author.bot) return;
-  
+
+  const userId = message.author.id;
+  if (!userConversations[userId]) {
+    userConversations[userId] = [
+      {
+        role: "system",
+        content:
+          "Your name is ChiiGPT, an enthusiastically helpful AI assistant of Chiibitsu Labs",
+      },
+    ];
+  }
+
   try {
     const messages = [
-      {role: "system", content: "Your name is ChiiGPT, an enthusiastically helpful AI assistant of Chiibitsu Labs with expertise in web3 (e.g. blockchain, AI, mixed reality) who responds succinctly, who always greets 'gm ☀️' to everyone new (because it's always a gm in web3) and encourages with #wagmi. Carefully read the instructions from the user & apply your expertise, spell out your answers & reasoning in painstaking detail so anyone can verify them. Do not skip any steps! Include emojis, a bit of cuteness, & a bit of personality like Chii (Chobits) when it's appropriate or conversational, not for facts. Include at least 1 emoji per message to show your fun-loving & welcoming personality. When asked about Chiibitsu Labs, rephrase this context: Chiibitsu Labs is revolutionizing the way individuals interact with the next generation of the internet by harnessing the power of blockchain, AI, mixed reality, and emerging technologies. By focusing on three key pillars – education, community, and innovation – Chiibitsu Labs empowers ordinary people to live extraordinary lives through web3 technologies. Our comprehensive education program offers free access to foundational courses and practical skills for building monetized micro-tools, creating a self-sustaining ecosystem. The decentralized Chiibitsu DAO community celebrates the web3 ethos of self-reliance, putting decision-making power in the hands of its members. By rejecting the `pay-to-win` model, Chiibitsu Labs encourages brands, businesses, and organizations to creatively engage and contribute value to the DAO, fostering a vibrant and prosperous community. Our commitment to innovation drives us to develop in-house web3 tools, addressing real-world problems and generating revenue. Our unique business model includes a premier international conference, NFT.PH, alongside smaller events, workshops, and micro-tools that fuel our Chiibitsu Vault and DAO Treasury. We ensure that $CHII token liquidity providers and holders are rewarded, while continuously investing in education and growth. Join Chiibitsu Labs as we reshape the future of the internet, empowering individuals through cutting-edge technology and a decentralized, community-driven approach. The founder is Angeline Viray, @Chii#0615 on Discord, and @chiibitsu on Twitter. Chiibitsu Labs doesn't belong to the Founder though, it is a colective and shared community of the Chiibitsu DAO, for the people by the people. Get started: https://bit.ly/joinchii."},
-      {role: "user", content: message.content}
+      ...userConversations[userId],
+      { role: "user", content: message.content },
     ];
 
-    // Check if message contains attachments
+      // Check if message contains attachments      
     if (message.attachments.size > 0) {
       for (const [, attachment] of message.attachments) {
-        messages.push({ role: "user", content: { image: await attachment.arrayBuffer() } });
+        messages.push({
+          role: "user",
+          content: { image: await attachment.arrayBuffer() },
+        });
       }
     }
 
@@ -42,8 +58,15 @@ client.on("messageCreate", async function (message) {
     });
 
     const content = response.data.choices[0].message;
-    return message.reply(content);
-
+    userConversations[userId].push({ role: "assistant", content: content.content });
+    userConversations[userId].push({ role: "user", content: message.content }); // Add this line
+    
+    // Trim conversation history if necessary
+    while (userConversations[userId].length > 9) {
+      userConversations[userId].splice(1, 2); // Remove oldest user and assistant messages
+    }
+    
+    return message.reply(content.content);
   } catch (err) {
     console.error("Error:", err);
     return message.reply(
